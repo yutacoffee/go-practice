cc3a354efb1164ebc27f7d26e1ba310fff7ce408
Yuta Nakahara <coffeebeans.77.black@gmail.com>
2021-01-25 23:27:29 +0900

    add goroutine directory

---
+++ goroutine/channel1/main.go
@@ -0,0 +1,30 @@
+package main 
+import(
+	"fmt"
+)
+
+func main() {
+
+	c := make(chan int) 
+
+	go func(s chan<- int) {
+
+		for i := 0; i < 5; i++ {
+			s <- i
+		}
+		close(s)
+	}(c)
+
+	for {
+
+		value, ok := <-c
+
+
+if !ok {
+	break
+}	
+
+fmt.Println(value)
+}
+
+}
---
+++ goroutine/channel2/main.go
@@ -0,0 +1,14 @@
+//チャネルのキャパシティとバッファリングされている要素数を取得
+package main
+
+import "fmt"
+
+func main() {
+	
+	c := make(chan int, 10)
+
+	c <- 0
+
+	fmt.Println("cap", cap(c))
+	fmt.Println("len", len(c))
+}
