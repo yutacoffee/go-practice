29b04c1638ed6b8556f1f7367a0bea0f348e2366
Yuta Nakahara <coffeebeans.77.black@gmail.com>
2021-01-25 17:15:29 +0900

    add map directory

---
+++ map/map2/main.go
@@ -0,0 +1,16 @@
+//マップの初期化
+package main
+
+import(
+	"fmt"
+)
+
+func main()  {
+	
+	capitals := map[string]string {
+		"日本": "東京",
+		"アメリカ": "ワシントンD.C.",
+		"中国": "北京"}
+
+	fmt.Println(capitals)
+}
